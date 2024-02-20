198fd2a656ed6aadcf24a89bc8fdf3afdc827982
Jamessmith9956 <jamessmith9956@gmail.com>
2024-02-20 14:43:02 +1100

    feat: Pipeline to organise python scripts. should be hosted as an appliaction or through databricks.

---
+++ Scripts/etl_pipeline.py
@@ -0,0 +1,16 @@
+
+# Extract
+
+## Retrieve transcript from google API 
+
+## Parse transcript
+
+# Transform
+
+## Calculate words per minute
+
+# Load
+
+## Save wpm to file
+
+## return wpm to user somehow
